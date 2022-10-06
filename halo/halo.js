c196184edf07f6c2ef9faa079241f3667d18d012
Justen Stall <justenstall@gmail.com>
2022-10-06 10:37:12 -0400

    axios tweaks

--- halo/halo.js
+++ halo/halo.js
@@ -22,37 +22,24 @@ import axios from 'axios';
 
 		matrix.clear().brightness(100);
 
-		const halodotapi = axios.create({
-			baseURL: 'https://cryptum.halodotapi.com/',
-			timeout: 1000,
-			headers: {
-				'Authorization': 'Bearer nc0tSzdnESGwGa84RNIL7aK17dLPUl4LjEkBHapZHMXk4rM75SDClGymRcHSIH2b',
-				'Content-Type': 'application/json'
+		const haloapi = {
+				baseURL: 'https://cryptum.halodotapi.com/games/hi',
+				// timeout: 1000,
+				headers: { 
+					'Authorization': env.HALO_TOKEN,
+					'Content-Type': 'application/json',
+				},
+				data: {},
 			}
-		});
+
+		const instance = axios.create(haloapi);
 
 		// make API request
 		// Make a request for a user with a given ID
 		let gamertag = 'JStall17'
-		axios.get(`https://cryptum.halodotapi.com/games/hi/appearance/players/${gamertag}`,
-			{
-				timeout: 1000,
-				headers: {
-					'Authorization': 'Bearer nc0tSzdnESGwGa84RNIL7aK17dLPUl4LjEkBHapZHMXk4rM75SDClGymRcHSIH2b',
-					'Content-Type': 'application/json'
-				}
-			}
-		)
-			.then(function (response) {
-				// handle success
-				console.log(response);
-			})
-			.catch(function (error) {
-				// handle error
-				console.log(error);
-			})
-			.finally(function () {
-				// always executed
+		instance.get(`/appearance/players/${gamertag}`, haloapi)
+			.then(response => {
+				console.log(response.data);
 			});
 
 		const baseBuffer = [...Array(matrix.width() * matrix.height() * 3).keys()];
