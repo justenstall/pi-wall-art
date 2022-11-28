d514180e74468d51c7046cdea1ddfefa138ab88e
Justen Stall <justenstall@gmail.com>
2022-11-28 10:18:55 -0500

    audio debugging

--- src/audio.py
+++ src/audio.py
@@ -2,9 +2,11 @@ import alsaaudio
 import time
 import io
 import numpy as np
+import librosa
+import soundfile as sf
 
 def visualizer():
-	device = 'default'
+	device = "hw:1,0"
 
 	# Open the device in nonblocking capture mode in mono, with a sampling rate of 44100 Hz 
 	# and 16 bit little endian samples
@@ -21,15 +23,26 @@ def visualizer():
 	
 	f = io.BytesIO()
 
+	# librosa.stream(f)
+	
+
 	while True:
 		# Read data from device
 		l, data = inp.read()
 
 		if l:
 			f.write(data)
+			sfdata, samplerate = sf.read(data)
+			print(f"Valid? {librosa.util.valid_audio(data.T)}")
 			time.sleep(.001)
 
-def sample_to_bars():
-	freq_bins = abs(array_split(fft(samples, self.count * 2), 2)[0]) / self.count
-	new_positions = np.clip(np.log10(freq_bins * 9 + 1), 0, 1)
+# def sample_to_bars(samples):
+# 	freq_bins = abs(np.array_split(np.fft(samples, self.count * 2), 2)[0]) / self.count
+# 	new_positions = np.clip(np.log10(freq_bins * 9 + 1), 0, 1)
 
+import pyaudio
+p = pyaudio.PyAudio()
+for ii in range(p.get_device_count()):
+   print(f"{ii}:{p.get_device_info_by_index(ii).get('name')}")
+
+# visualizer()
