05306a4f1ab130407406441a0758e102eaa30637
Justen Stall <justenstall@gmail.com>
2022-11-28 22:09:29 -0500

    organize deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+# Destroy old setup
 sudo rm -rf /usr/local/bin/pi-wall-art
-sudo cp -R . /usr/local/bin/pi-wall-art
-
-sudo cp rgbmatrix.sh /etc/init.d
-
 sudo /etc/init.d/rgbmatrix.sh stop
-sudo /etc/init.d/rgbmatrix.sh start
+sudo /etc/init.d/shutdown-button.sh stop
 
+# Copy new python files
+sudo cp -R . /usr/local/bin/pi-wall-art
 sudo cp shutdown-button/shutdown-button.py /usr/local/bin
 
+# Copy new daemon files
+sudo cp rgbmatrix.sh /etc/init.d
 sudo cp shutdown-button/shutdown-button.sh /etc/init.d
 
-sudo /etc/init.d/shutdown-button.sh stop
+# Start up new setup
+sudo /etc/init.d/rgbmatrix.sh start
 sudo /etc/init.d/shutdown-button.sh start
